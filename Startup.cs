7663a0759267ac483f4af8b181964e64da932481
narongrit <narongritAcer@LAPTOP-acer>
2021-10-02 13:50:42 +0700

    make api works

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace SPF_Receipt
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();           
+            services.AddControllers();
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
--- ViewModel/BaseResponse.cs
+++ ViewModel/BaseResponse.cs
@@ -10,7 +10,7 @@ namespace SPF_Receipt.ViewModel
         public bool IsSuccess { get; set; }
         public string Message { get; set; }
 
-        public BaseResponse(bool isSuccess = true, string message = "Success")
+        public BaseResponse(bool isSuccess = true, string message = "ทำงานสำเร็จ")
         {
             IsSuccess = isSuccess;
             Message = message;
