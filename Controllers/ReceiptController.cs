45e5087243d689927f9e80f3c170e175972d56ab
narongrit <narongritAcer@LAPTOP-acer>
2021-10-05 20:41:15 +0700

    initial add receipt

---
+++ Controllers/ReceiptController.cs
@@ -0,0 +1,68 @@
+﻿using FastReport;
+using FastReport.Export.PdfSimple;
+using Microsoft.AspNetCore.Mvc;
+using SPF_Receipt.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPF_Receipt.Controllers
+{
+    [ApiController]
+    [Route("receipts/[action]")]
+    public class ReceiptController : ControllerBase
+    {
+        [HttpGet]
+        public FileContentResult Download()
+        {
+            byte[] content;
+            using (var memoryStream = new MemoryStream())
+            {
+                var report = new Report();
+                var buildPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+                report.Load($"{buildPath}/Reports/ReceiptReport.frx");
+                var number = report.GetParameter("ReceiptNumber");
+                number.Value = "612345678";
+
+                var list = new List<ProductReceipt<string>>
+                {
+                    new ProductReceipt<string>
+                    {
+                        Price = "1234",
+                        ProductName = "test",
+                        Qty = "55",
+                        TotalPrice = "777",
+                        UnitName = "444"
+                    },
+                    new ProductReceipt<string>
+                    {
+                        Price = "1234",
+                        ProductName = "test",
+                        Qty = "55",
+                        TotalPrice = "777",
+                        UnitName = "444"
+                    }
+                };
+
+                report.RegisterData(list, "ProductList");
+
+
+                report.Prepare();
+
+                var pdfExport = new PDFSimpleExport();
+                pdfExport.Export(report, memoryStream);
+
+                content = memoryStream.ToArray();
+            }
+
+
+            return new FileContentResult(content, "application/pdf")
+            {
+                FileDownloadName = $"Receipt_{DateTime.Now.ToString("yyyyMMdd_HHmm")}.pdf"
+            };
+        }
+    }
+}
---
+++ ViewModel/ReceiptModel.cs
@@ -0,0 +1,30 @@
+﻿using SPF_Receipt.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPF_Receipt.ViewModel
+{
+    public class ReceiptModel
+    {
+        public string ReceiptNumber { get; set; }
+        public string ReceiptDateString { get; set; }
+        public string DueDateString { get; set; }
+        public Payment Payment { get; set; }
+        public Customer Customer { get; set; }
+        public IEnumerable<ProductReceipt<double>> ProductList { get; set; }
+        public double Total { get; set; }
+        public double Tax { get; set; }
+        public double GrandTotal { get; set; }
+    }
+
+    public class ProductReceipt<TMoney>
+    {
+        public string ProductName { get; set; }
+        public string UnitName  { get; set; }
+        public string Qty { get; set; }
+        public TMoney Price { get; set; }
+        public TMoney TotalPrice { get; set; }
+    }
+}
